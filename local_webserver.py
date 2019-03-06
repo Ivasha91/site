13dd7d52f9ae375d266771ac9c050276c4b5a59f
Ivasha91 <2duraka@gmail.com>
2019-03-06 19:42:00 +0700

    Заменил таблицу на ДИВ

---
+++ local_webserver.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, session, g, redirect, url_for,abort, flash, json, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST','GET'])
+def index():
+    author = "Brax"
+    return render_template('index.html', author=author)
+
+if __name__ == "__main__":
+    app.Debug=True
+    app.run(host='0.0.0.0')
