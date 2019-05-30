75868149b851df00742dbd8d2e077a7596748122
Ivasha91 <2duraka@gmail.com>
2019-05-30 07:50:29 +0700

    добавил тяни надись

--- static/js/jquery.twentytwenty.js
+++ static/js/jquery.twentytwenty.js
@@ -6,7 +6,7 @@
       orientation: 'horizontal',
       before_label: 'Before',
       after_label: 'After',
-      no_overlay: false,
+      no_overlay: true,
       move_slider_on_hover: false,
       move_with_handle_only: true,
       click_to_move: false
@@ -27,7 +27,7 @@
       }
       var beforeImg = container.find("img:first");
       var afterImg = container.find("img:last");
-      container.append("<div class='twentytwenty-handle'></div>");
+      container.append("<div class='twentytwenty-handle'>        Тяни</div>");    //текст к слайдеру
       var slider = container.find(".twentytwenty-handle");
       slider.append("<span class='twentytwenty-" + beforeDirection + "-arrow'></span>");
       slider.append("<span class='twentytwenty-" + afterDirection + "-arrow'></span>");
