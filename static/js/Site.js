1cb5980d51e89a5f9216ee8024187c372167fa38
Ivasha91 <2duraka@gmail.com>
2019-06-07 07:50:53 +0700

    бла бла

--- static/js/Site.js
+++ static/js/Site.js
@@ -1,4 +1,17 @@
 //Скрипт для всплывающего окна Бронирования
-$('#Sixth_Block').click(function(){
+$('.js-button').click(function(){
 	$('#Sixth_Block').addClass('active')
 })
+
+$(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы    
+        PopUpHide();
+    });
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#Sixth_Block").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#Sixth_Block").hide();
+    }
